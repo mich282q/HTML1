df704b615c95fd12423a782ce5d3e7b7de2cf9b1
Michael Trans <mich282q@edu.easj.dk>
2018-09-19 13:56:34 +0200

    test.harboe

---
+++ Mail.php
@@ -0,0 +1,23 @@
+
+<?php
+$Navn =$_POST['Dit Navn'];
+$MedlemsNr =$_POST['MedlemsNr'];
+$cola =$_POST['Cola 05'];
+$Topform =$_POST['Topform'];
+$Classic =$_POST['Classic'];
+$Pilser =$_POST['Pilser'];
+if(isset($_POST['send'])){
+
+    $to = "michael_trans@hotmail.com";
+    $subject = "Personalekøb";
+    $txt = "Navn " + $Navn + "\n" + " MedlemsNr " + $MedlemsNr + "\n" + " Antal cola på 0,5 liter= " + $cola + 
+    " Antal Topform = " + $Topform + " Antal Classic " + $Classic + " Antal Pilser " + $Pilser;
+    
+    $headers = "From: it@michael-trans.dk" . "\r\n" ;
+//echo $txt;
+    mail($to,$subject,$txt,$headers);
+}
+// the message
+
+?>
+
