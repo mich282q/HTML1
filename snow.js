4c117184f6bd5fecd58b0ee7408563099b7992e7
Michael Trans <mich282q@edu.easj.dk>
2018-11-29 21:33:14 +0100

    fixet fejl i koden

--- snow.js
+++ snow.js
@@ -1,51 +1,51 @@
 var canvas = document.querySelector('.snow'),
-    ctx = canvas.getContext('2d'),
-    windowW = window.innerWidth,
-    windowH = window.innerHeight,
-    numFlakes = 400,
-    flakes = [];
+  ctx = canvas.getContext('2d'),
+  windowW = window.innerWidth,
+  windowH = window.innerHeight,
+  numFlakes = 400,
+  flakes = [];
 
 function Flake(x, y) {
   var maxWeight = 5,
-      maxSpeed = 3;
-  
+    maxSpeed = 3;
+
   this.x = x;
   this.y = y;
   this.r = randomBetween(0, 1);
   this.a = randomBetween(0, Math.PI);
   this.aStep = 0.01;
 
-  
+
   this.weight = randomBetween(2, maxWeight);
   this.alpha = (this.weight / maxWeight);
   this.speed = (this.weight / maxWeight) * maxSpeed;
-  
-  this.update = function() {
+
+  this.update = function () {
     this.x += Math.cos(this.a) * this.r;
     this.a += this.aStep;
-    
+
     this.y += this.speed;
   }
-  
+
 }
 
 function init() {
   var i = numFlakes,
-      flake,
-      x,
-      y;
-  
+    flake,
+    x,
+    y;
+
   while (i--) {
     x = randomBetween(0, windowW, true);
     y = randomBetween(0, windowH, true);
-    
-    
+
+
     flake = new Flake(x, y);
     flakes.push(flake);
   }
-  
+
   scaleCanvas();
-  loop();  
+  loop();
 }
 
 function scaleCanvas() {
@@ -55,23 +55,23 @@ function scaleCanvas() {
 
 function loop() {
   var i = flakes.length,
-      z,
-      dist,
-      flakeA,
-      flakeB;
-  
+    z,
+    dist,
+    flakeA,
+    flakeB;
+
   // clear canvas
   ctx.save();
   ctx.setTransform(1, 0, 0, 1, 0, 0);
   ctx.clearRect(0, 0, windowW, windowH);
   ctx.restore();
-  
+
   // loop of hell
   while (i--) {
-    
+
     flakeA = flakes[i];
     flakeA.update();
-    
+
 
     /*for (z = 0; z < flakes.length; z++) {
       flakeB = flakes[z];
@@ -85,17 +85,17 @@ function loop() {
       }
     }*/
 
-    
+
     ctx.beginPath();
     ctx.arc(flakeA.x, flakeA.y, flakeA.weight, 0, 2 * Math.PI, false);
     ctx.fillStyle = 'rgba(255, 255, 255, ' + flakeA.alpha + ')';
     ctx.fill();
-    
+
     if (flakeA.y >= windowH) {
       flakeA.y = -flakeA.weight;
-    }  
+    }
   }
-  
+
   requestAnimationFrame(loop);
 }
 
@@ -111,9 +111,9 @@ function randomBetween(min, max, round) {
 
 function distanceBetween(vector1, vector2) {
   var dx = vector2.x - vector1.x,
-      dy = vector2.y - vector1.y;
+    dy = vector2.y - vector1.y;
 
-  return Math.sqrt(dx*dx + dy*dy);
+  return Math.sqrt(dx * dx + dy * dy);
 }
 
 init();
